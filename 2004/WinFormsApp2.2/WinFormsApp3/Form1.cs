1cabd3a3fbff00b355b2de3a6edd6bf1b39ac536
camagma <ivan.zabila11@gmail.com>
2026-04-20 10:10:47 +0300

    saper

---
+++ 2004/WinFormsApp2.2/WinFormsApp3/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        const int mapSize = 10;
+        const int cellSize = 35;
+        const int minesCount = 12;
+
+        int[,] map = new int[mapSize, mapSize];
+        Button[,] buttons = new Button[mapSize, mapSize];
+        int cellsOpened = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Сапер";
+
+            this.KeyPreview = true;
+
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            this.Controls.Clear();
+            cellsOpened = 0;
+            Array.Clear(map, 0, map.Length);
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    Button btn = new Button();
+                    btn.Size = new Size(cellSize, cellSize);
+                    btn.Location = new Point(j * cellSize, i * cellSize);
+                    btn.Tag = new Point(i, j);
+                    btn.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+                    btn.FlatStyle = FlatStyle.Standard;
+
+                    btn.Click += OnCellClick;
+
+                    buttons[i, j] = btn;
+                    this.Controls.Add(btn);
+                }
+            }
+
+            PlaceMines();
+            CalculateNumbers();
+        }
+
+        private void PlaceMines()
+        {
+            Random rnd = new Random();
+            int placed = 0;
+            while (placed < minesCount)
+            {
+                int x = rnd.Next(mapSize);
+                int y = rnd.Next(mapSize);
+                if (map[x, y] != -1)
+                {
+                    map[x, y] = -1;
+                    placed++;
+                }
+            }
+        }
+
+        private void CalculateNumbers()
+        {
+            for (int i = 0; i < mapSize; i++)
+                for (int j = 0; j < mapSize; j++)
+                    if (map[i, j] != -1)
+                        map[i, j] = CountAround(i, j);
+        }
+
+        private int CountAround(int x, int y)
+        {
+            int count = 0;
+            for (int i = x - 1; i <= x + 1; i++)
+                for (int j = y - 1; j <= y + 1; j++)
+                    if (i >= 0 && i < mapSize && j >= 0 && j < mapSize && map[i, j] == -1)
+                        count++;
+            return count;
+        }
+
+        private void OnCellClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Point p = (Point)btn.Tag;
+
+            if (map[p.X, p.Y] == -1)
+            {
+                btn.BackColor = Color.Red;
+                btn.Text = "b";
+                MessageBox.Show("Ой! Це була міна.");
+                StartNewGame();
+            }
+            else
+            {
+                OpenCell(p.X, p.Y);
+                CheckWin();
+            }
+        }
+
+        private void OpenCell(int x, int y)
+        {
+            if (x < 0 || x >= mapSize || y < 0 || y >= mapSize || !buttons[x, y].Enabled) return;
+
+            buttons[x, y].Enabled = false;
+            buttons[x, y].BackColor = Color.WhiteSmoke;
+            cellsOpened++;
+
+            if (map[x, y] > 0)
+            {
+                buttons[x, y].Text = map[x, y].ToString();
+                if (map[x, y] == 1) buttons[x, y].ForeColor = Color.Blue;
+                if (map[x, y] == 2) buttons[x, y].ForeColor = Color.Green;
+                if (map[x, y] >= 3) buttons[x, y].ForeColor = Color.DarkRed;
+            }
+            else
+            {
+                for (int i = x - 1; i <= x + 1; i++)
+                    for (int j = y - 1; j <= y + 1; j++)
+                        OpenCell(i, j);
+            }
+        }
+
+        private void CheckWin()
+        {
+            if (cellsOpened == (mapSize * mapSize) - minesCount)
+            {
+                MessageBox.Show("Перемога! Ви розмінували поле.");
+                StartNewGame();
+            }
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.R)
+            {
+                StartNewGame();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
---
+++ 2004/WinFormsApp2/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class MainForm : Form
+    {
+        private const int GridSize = 5;
+        private const int MineCount = 5;
+        private Button[,] buttons = new Button[GridSize, GridSize];
+        private bool[,] mines = new bool[GridSize, GridSize];
+
+        public MainForm()
+        {
+            this.Text = "Простий Сапер (5x5)";
+            this.Size = new Size(300, 350);
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            // Очищення поля, якщо гра перезапускається
+            this.Controls.Clear();
+
+            Random rnd = new Random();
+            int placedMines = 0;
+
+            // Генерація мін
+            while (placedMines < MineCount)
+            {
+                int r = rnd.Next(GridSize);
+                int c = rnd.Next(GridSize);
+                if (!mines[r, c])
+                {
+                    mines[r, c] = true;
+                    placedMines++;
+                }
+            }
+
+            // Створення кнопок
+            for (int i = 0; i < GridSize; i++)
+            {
+                for (int j = 0; j < GridSize; j++)
+                {
+                    buttons[i, j] = new Button
+                    {
+                        Size = new Size(50, 50),
+                        Location = new Point(i * 50 + 20, j * 50 + 20),
+                        Tag = new Point(i, j), // Зберігаємо координати в Tag
+                        Font = new Font("Arial", 12, FontStyle.Bold)
+                    };
+
+                    buttons[i, j].Click += Button_Click;
+                    this.Controls.Add(buttons[i, j]);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Point p = (Point)btn.Tag;
+
+            if (mines[p.X, p.Y])
+            {
+                btn.Text = "b";
+                btn.BackColor = Color.Red;
+                MessageBox.Show("БАБАХ! Ви програли.", "Кінець гри");
+                ResetGame();
+            }
+            else
+            {
+                btn.Text = "✓";
+                btn.BackColor = Color.LightGray;
+                btn.Enabled = false; // Вимикаємо кнопку після успішного ходу
+            }
+        }
+
+        private void ResetGame()
+        {
+            mines = new bool[GridSize, GridSize];
+            InitializeGame();
+        }
+    }
+}
+
